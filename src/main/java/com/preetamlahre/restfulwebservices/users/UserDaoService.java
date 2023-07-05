59fac3fedec2b9e263132162fcc62f96b435ceca
Preetam <nihal@Nihal.local>
2023-07-05 16:28:15 +0530

    added delete request

--- src/main/java/com/preetamlahre/restfulwebservices/users/UserDaoService.java
+++ src/main/java/com/preetamlahre/restfulwebservices/users/UserDaoService.java
@@ -22,6 +22,10 @@ public class UserDaoService {
 		Predicate<? super User> predicate = user -> user.getId()==id; 
 		return users.stream().filter(predicate).findFirst().get();
 	}
+	public void deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId()==id;
+		users.removeIf(predicate);
+	}
 	public User save(User user) {
 		user.setId(++usersCount);
 		 users.add(user);
